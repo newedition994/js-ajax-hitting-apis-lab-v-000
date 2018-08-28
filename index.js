bae7580731ef7cc5ebba7b598196fa16a050a8ce
Breyanna Vass <breyanna.bvass@gmail.com>
2018-08-28 21:43:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getRepositories() {
   const req = new XMLHttpRequest
   let username = document.getElementById("username").value
   req.addEventListener("load", displayRepositories)
-  req.open("GET", 'https://api.github.com/users/${username}/repos')
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
 }
 
